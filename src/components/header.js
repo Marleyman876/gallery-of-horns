2a8787a6d9c89383b7f7e372a57ff2cbfe283543
Marleyman876 <grant_garfield@yahoo.com>
2021-03-24 22:46:28 -0700

    changed header

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React from "react";
 class Header extends React.Component{
   render(){
     return (
-      <h1> I will use this header to rank my fave Marvel Characters!  </h1>
+      <h1> Horn Beast </h1>
     )
   }
 }
